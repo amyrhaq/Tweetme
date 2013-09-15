43f714f162ecf0f58571db0ca0ca0089221dbd3a
Your Name Here <amyr@amyrhaq.com>
2013-09-14 20:23:20 -0700

    Start an example Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetme::Application.config.secret_token = '7cc482cfce8a8856d8a8e3b132729ca0042a808dbf24689e50c32f8127f1fc3b0f11d0978906231dd3703ae8e39f6e59aac226ec5ca025211c19b8647da3171e'
